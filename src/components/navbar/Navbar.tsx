4f0545f9a09401cbd1a5c2b684e2d146504d4036
Lok <oui19867@gmail.com>
2023-08-30 22:09:30 +0800

    sign in/out

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -13,54 +13,92 @@ import {
 } from '@chakra-ui/react'
 import { FiBell, FiSearch } from 'react-icons/fi'
 import { DocumentPopover } from './DocumentPopover'
+import React, { useState, useEffect } from "react"
 import { Logo } from './Logo'
 import { MobileDrawer } from './MobileDrawer'
+import {Link } from "react-router-dom";
+import { Auth } from 'aws-amplify';
 
-export const Navbar = () => (
-  <Box as="section" minH="lg">
-    <Box borderBottomWidth="1px" bg="bg.accent.default" position="relative" zIndex="tooltip">
-      <Container py="4">
-        <HStack justify="space-between" spacing="8">
-          <HStack spacing="10">
-            <HStack spacing="3">
-              <MobileDrawer />
-              <Logo />
+async function checkUserLogin() {
+  try {
+    const user = await Auth.currentAuthenticatedUser();
+    return user.username;
+  } catch (err) {
+    return null;
+  }
+}
+export const Navbar = () => {
+  const [username, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const user = await checkUserLogin();
+      setUsername(user);
+    }
+    fetchData();
+  }, []);
+
+  async function handleSignOut() {
+    try {
+      await Auth.signOut();
+      setUsername(null);
+    } catch (error) {
+      console.log('error signing out: ', error);
+    }
+  }
+
+  return (
+    <Box as="section" minH="lg">
+      <Box borderBottomWidth="1px" bg="bg.accent.default" position="relative" zIndex="tooltip">
+        <Container py="4">
+          <HStack justify="space-between" spacing="8">
+            <HStack spacing="10">
+              <HStack spacing="3">
+                <MobileDrawer />
+                <Logo />
+              </HStack>
+              <ButtonGroup
+                size="lg"
+                variant="text.accent"
+                colorScheme="gray"
+                spacing="8"
+                display={{ base: 'none', lg: 'flex' }}
+              >
+                <Button>Dashbaord</Button>
+                <Button>Analysis</Button>
+                <DocumentPopover />
+                <Button>History</Button>
+            
+              </ButtonGroup>
             </HStack>
-            <ButtonGroup
-              size="lg"
-              variant="text.accent"
-              colorScheme="gray"
-              spacing="8"
-              display={{ base: 'none', lg: 'flex' }}
-            >
-              <Button>Dashbaord</Button>
-              <Button>Analysis</Button>
-              <DocumentPopover />
-              <Button>History</Button>
-            </ButtonGroup>
-          </HStack>
-          <HStack spacing={{ base: '2', md: '4' }}>
-            <InputGroup maxW="2xs" display={{ base: 'none', md: 'inline-flex' }}>
-              <InputLeftElement>
-                <Icon as={FiSearch} color="fg.accent.muted" fontSize="lg" />
-              </InputLeftElement>
-              <Input placeholder="Search" variant="filled.accent" />
-            </InputGroup>
-            <ButtonGroup variant="tertiary.accent" spacing="1">
-              <IconButton
-                icon={<FiSearch />}
-                aria-label="Serach"
-                display={{ base: 'flex', lg: 'none' }}
-                isRound
-              />
-              <IconButton icon={<FiBell />} aria-label="Show notification" isRound />
-            </ButtonGroup>
-            <Avatar boxSize="10" src="https://i.pravatar.cc/300" />
-          </HStack>
-        </HStack>
-      </Container>
-    </Box>
-  </Box>
-)
+            <HStack spacing={{ base: '2', md: '4' }}>
+              <InputGroup maxW="2xs" display={{ base: 'none', md: 'inline-flex' }}>
+                <InputLeftElement>
+                  <Icon as={FiSearch} color="fg.accent.muted" fontSize="lg" />
+                </InputLeftElement>
+                <Input placeholder="Search" variant="filled.accent" />
+              </InputGroup>
+              <ButtonGroup variant="tertiary.accent" spacing="1">
+              {username ? (
+                  <>
+                    <p> {username}.</p>
+                    <Button onClick={handleSignOut}>Sign Out</Button>
+                  </>
+                ) : (
+                  <Link to="/login">Sign In</Link>
+                )}
+                <IconButton icon={<FiBell />} aria-label="Show notification" isRound />
+              </ButtonGroup>
+              <Avatar boxSize="10" src="<https://i.pravatar.cc/300>" />
 
-export default Navbar
+
+            </HStack>
+          </HStack>
+          
+        </Container>
+      </Box>
+    </Box>
+  );
+};
+
+export default Navbar;
