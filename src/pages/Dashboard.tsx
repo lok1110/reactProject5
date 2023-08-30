741624ba66a6bdf3e73ce4475e9343b5002905ba
Lok <oui19867@gmail.com>
2023-08-30 21:59:55 +0800

    sign button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,19 +1,26 @@
-import React  from "react"
-import { useNavigate,Link } from "react-router-dom";
+import { Auth } from 'aws-amplify';
+import React, { useState, useEffect } from "react"
+import { useNavigate, Link } from "react-router-dom";
+
 export interface IHomePageProps {}
 
-const Dashboard: React.FunctionComponent<IHomePageProps> = (props) =>{
-  const navigate = useNavigate();
-  return (
-   
+interface ResendConfCodeParameters {
+  username: string;
+}
 
+
+
+const Dashboard: React.FunctionComponent<IHomePageProps> = (props) => {
+  const navigate = useNavigate();
+
+  return (
     <div>
-       <Link to="/Profile">go to the </Link>
+  
+      <Link to="/Profile">Go to Profile</Link>
       <button onClick={() => navigate('/layout/55')}>Go to layout</button>
-      <h1>Dashboard</h1></div>
+      <h1>Dashboard</h1>
+    </div>
   );
 };
-
-
 
 export default Dashboard;
